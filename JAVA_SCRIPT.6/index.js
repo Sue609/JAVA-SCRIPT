bb34a31f5e3d47eba3e1d4f158bb4db19607803a
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-05-10 20:32:06 +0300

    formatting strings in Java Script

---
+++ JAVA_SCRIPT.6/index.js
@@ -0,0 +1,8 @@
+let userName = "  Susan Kamau     ";
+let nameLength = userName.length;
+
+console.log(userName);
+console.log(userName.charAt(4));
+
+userName = userName.trim();
+console.log(userName);
