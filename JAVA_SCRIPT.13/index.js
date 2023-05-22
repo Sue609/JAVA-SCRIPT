8809631ffb589d693ce3176b6f04e7cfe887ef1d
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-05-22 15:28:38 +0300

    JAVA SCRIPT code for practice with a range of topics

---
+++ JAVA_SCRIPT.13/index.js
@@ -0,0 +1,18 @@
+let userName = "Sue";
+let age = 21;
+
+HappyBirthday();
+HappyBirthday();
+HappyBirthday();
+HappyBirthday();
+
+function HappyBirthday(){
+    //let name = window.prompt("What is your name");
+
+    console.log("Happy Birthday to you!");
+    console.log("Happy Birthday to you!");
+    console.log("Happy Birthday dear " + userName);
+    console.log("Happy Birthday to you!");
+    console.log("How old are you now?", age);
+
+}
